0403673b19ecac7af63afc821ea0aea36635e4e9
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2025-12-16 15:40:20 +0300

    Cell düzenlendi

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,22 +1,40 @@
 import type { CellProps } from "../types/props/";
 
-export default function Cell({ cell, onClick }: CellProps) {
+export default function Cell({ cell, onClick, onRightClick }: CellProps) {
     const handleClick = () => {
         onClick(cell.row, cell.col);
+    };
+
+    const handleRightClick = (e: React.MouseEvent) => {
+        e.preventDefault();
+        onRightClick(cell.row, cell.col);
     };
 
     let content = "";
 
     if (cell.isRevealed) {
-        if (cell.isMine) content = "💣";
-        else if (cell.adjacentMines > 0) content = String(cell.adjacentMines);
+        if (cell.isMine) {
+            content = "💣";
+        } else if (cell.adjacentMines > 0) {
+            content = String(cell.adjacentMines);
+        }
+    } else if (cell.isFlagged) {
+        content = "🚩";
     }
 
     return (
         <button
-            className="cell"
             onClick={handleClick}
+            onContextMenu={handleRightClick}
             disabled={cell.isRevealed}
+            style={{
+                width: 32,
+                height: 32,
+                fontSize: 16,
+                cursor: "pointer",
+                backgroundColor: cell.isRevealed ? "#ddd" : "#aaa",
+                border: "1px solid #555",
+            }}
         >
             {content}
         </button>
