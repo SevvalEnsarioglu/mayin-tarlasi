0dea2910c18dca4cac57e96e22e68c62336ab15e
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2025-12-16 16:58:51 +0300

    Css tasarımı yapıldı

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import SetupScreen from "./components/SetupScreen";
 import GameScreen from "./components/GameScreen";
 import type { GameSettings } from "./types";
+import './App.css';
+
 
 export default function App() {
     const [settings, setSettings] = useState<GameSettings | null>(null);
